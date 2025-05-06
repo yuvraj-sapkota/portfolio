5b52a6937cc64c45d83ff43e4d2b90f3179dac31
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2025-05-07 00:03:21 +0545

    worked on hero section

--- frontend/src/components/HeaderSeaction.jsx
+++ frontend/src/components/HeaderSeaction.jsx
@@ -5,7 +5,7 @@ import { MdOutlineStarPurple500 } from "react-icons/md";
 const HeaderSection = () => {
   return (
     <>
-      <div className="mx-auto max-w-[900px]   flex flex-col items-center justify-center gap-2 mb-4 md:mb-[40px] ">
+      <div className="mx-auto max-w-[900px] px-4   flex flex-col items-center justify-center gap-2 mb-4 md:mb-[40px] ">
         <div className="border py-[8px] px-[24px] rounded-full font-bold">
           <p>Hello!</p>
         </div>
@@ -17,31 +17,34 @@ const HeaderSection = () => {
         </div>
       </div>
 
-      <div className="max-w-[1300px] mx-auto flex md:items-start justify-between flex-col items-center gap-4">
+      <div className="max-w-[1300px] px-4 mx-auto flex md:items-start justify-between flex-col md:flex-row items-center gap-4">
         {/* quotes  */}
-        <div className="border  border-black order-2">
+        <div className="order-2 md:order-1 ">
           <div className="mb-6">
             <FaQuoteLeft />
           </div>
-          <p className="max-w-[280px] -tracking-tighter ">
+          <p className="max-w-[280px] -tracking-tighter font-serif">
             Jenny’s Exceptional product design ensure our website’s success.
             Highly Recommended
           </p>
         </div>
         {/* image  */}
-        <div className="w-[250px] h-[250px] rounded-full bg-[#FD853A] overflow-hidden object-cover border  border-black order-1">
+        <div className="w-[250px] md:w-[400px] h-[250px] md:h-[400px] rounded-full bg-[#FD853A] overflow-hidden object-cover order-1 md:order-2">
           <img src={me} alt="" />
         </div>
 
         {/* experience  */}
-        <div  className="border  border-black order-3">
+        <div className="order-3 ">
           <div className="flex items-center">
             <MdOutlineStarPurple500 className="text-[#FD853A]" />
             <MdOutlineStarPurple500 className="text-[#FD853A]" />
             <MdOutlineStarPurple500 className="text-[#FD853A]" />
             <MdOutlineStarPurple500 className="text-[#FD853A]" />
           </div>
-          <p className="font-bold text-2xl text-end">2 Years plus <br /> <span className="font-normal text-sm text-end"> Experience </span></p>
+          <p className="font-bold text-2xl text-end font-serif">
+            2 Years plus <br />{" "}
+            <span className="font-normal text-sm text-end"> Experience </span>
+          </p>
         </div>
       </div>
     </>
