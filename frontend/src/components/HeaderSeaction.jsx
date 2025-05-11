565b62a7835ec47932b1ce181b56804c824cb68f
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2025-05-11 19:29:12 +0545

    working on the hersection

--- frontend/src/components/HeaderSeaction.jsx
+++ frontend/src/components/HeaderSeaction.jsx
@@ -17,20 +17,26 @@ const HeaderSection = () => {
         </div>
       </div>
 
-      <div className="max-w-[1300px] px-4 mx-auto flex  flex-col md:flex-row items-start justify-between  gap-4">
+      <div className="max-w-[1300px] px-4 mx-auto flex  flex-col md:flex-row items-center justify-between  gap-4">
         {/* quotes  */}
         <div className="order-2 md:order-1 ">
           <div className="mb-6">
             <FaQuoteLeft />
           </div>
-          <p className="md:max-w-[200px] -tracking-tighter font-serif">
+          <p className="max-w-[400px] md:max-w-[200px] -tracking-tighter font-serif">
             Yuvraj’s exceptional web development made our website fast,
             functional, and beautiful. Highly recommended!
           </p>
         </div>
         {/* image  */}
-        <div className="w-[350px] md:w-[400px] h-[350px] md:h-[400px] rounded-full bg-[#FD853A] overflow-hidden object-cover order-1 md:order-2">
-          <img src={me} alt="" />
+        <div className="relative border border-red-500">
+          <div className="w-[350px] md:w-[400px] h-[350px] md:h-[400px] rounded-full bg-[#FD853A] overflow-hidden object-cover order-1 md:order-2">
+            <img src={me} alt="" />
+          </div>
+          <div className="absolute bottom-0 right-1/2 rounded-full bg-orange-200 p-1">
+            <button className="custome-orange">Portfolio</button>
+            <button>Hire me</button>
+          </div>
         </div>
 
         {/* experience  */}
