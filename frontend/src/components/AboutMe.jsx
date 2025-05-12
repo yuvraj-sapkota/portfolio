948f4ef00a5bbba6b6a4cd7c37e2cca39bc13376
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2025-05-12 16:38:29 +0545

    working started on about me section

--- frontend/src/components/AboutMe.jsx
+++ frontend/src/components/AboutMe.jsx
@@ -1,16 +1,21 @@
 const AboutMe = () => {
   return (
     <>
-      <div className="max-w-[1300px] mx-auto bg-[#FD853A] flex mt-4">
-        <div className="h-96  border border-black text-white">
-          2 years of experice
+     <div className="max-w-[1300px] mx-auto mt-18 px-4">
+         <div className=" bg-[#FD853A] flex items-center justify-center gap-4  py-12 px-4">
+        <div className="  bg-white rounded-2xl p-4">
+         
+          <div className=" bg-[#FD853A] rounded-full w-48 h-48 flex items-center justify-center text-white">
+            <p className="text-8xl font-extrabold ">2+</p>
+          </div>
         </div>
-        <div className="bg-blue-500">
+        <div className="   w-[400px] flex flex-col  gap-4">
             
-          <div>mlfd</div>
-          <div>jilarj</div>
+          <div className="bg-white rounded-2xl">mlfd</div>
+          <div className="bg-white rounded-2xl">jilarj</div>
         </div>
       </div>
+     </div>
 
       <div className="h-96">
 
